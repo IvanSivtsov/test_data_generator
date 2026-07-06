b1a5784237e402f4f317ccedaa9f26c8736cfef3
Ivan Sivtsov <ivansivtsov94@gmail.com>
2026-07-06 20:09:55 +0200

    Feature: New reqs

--- TestDataGenerator.py
+++ TestDataGenerator.py
@@ -15,32 +15,41 @@ ages = [
     42,
     19
 ]
+
+
 MAX_AGE = 120
 MIN_AGE = 0
+PREMIUM_AGE = 30
 premium_users_counter = 0
 basic_users_counter = 0
 testdata_linebreak = "\n--------------------\n"
 
-
-for i in range(len(names)):
-    age = ages[i]
-    name = names[i]
-    print(f"User #{i + 1}")
-    print(f"Name: {names[i]}")
-    print(f"Age: {ages[i]}")
-    if  30 <= age < MAX_AGE:
-        user_status = "Premium"
-        premium_users_counter += 1
-    elif MIN_AGE < age < 30:
-        user_status = "Basic"
-        basic_users_counter += 1
+users_amount = input("How many users do you want?\n")
+if users_amount.isdigit():
+    users_amount = int(users_amount)
+    if users_amount <= len(names) and users_amount > 0:
+        for i in range(users_amount):
+            age = ages[i]
+            name = names[i]
+            print(f"User #{i + 1}")
+            print(f"Name: {name}")
+            print(f"Age: {age}")
+            if PREMIUM_AGE <= age < MAX_AGE:
+                user_status = "Premium"
+                premium_users_counter += 1
+            elif MIN_AGE < age < PREMIUM_AGE:
+                user_status = "Basic"
+                basic_users_counter += 1
+            else:
+                user_status = "Unknown"
+                print("Age is not valid")
+            print(f"Status: {user_status}")
+            print(testdata_linebreak)
+        print("\n----------REPORT----------\n"
+            f"Total users: {users_amount}\n"
+            f"Premium users: {premium_users_counter}\n"
+            f"Basic users: {basic_users_counter}\n")
     else:
-        user_status = "Unknown"
         print("Age is not valid")
-    print(f"Status: {user_status}")
-    print(testdata_linebreak)
-
-print("\n----------REPORT----------\n"
-        f"Total users: {len(names)}\n"
-        f"Premium users: {premium_users_counter}\n"
-        f"Basic users: {basic_users_counter}\n")
+else:
+    print("Error: invalid input")
